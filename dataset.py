5b37352059e6d99b0b610eebd284e743869eb023
liyun <1062428799@qq.com>
2024-12-17 19:12:38 +0800

    加载5张图片，并且加上噪声附上可视化

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import torchvision
-from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Lambda, ToTensor
 import matplotlib.pyplot as plt
-import cv2
 
 def download_dataset():
     mnist = torchvision.datasets.MNIST(root='data/mnist', download=True)
@@ -12,17 +12,25 @@ def download_dataset():
 
     image_name = f"image_id_{id}_label_{label}"
     image_path = f'workdirs/{image_name}.jpg'
-    plt.imshow(img)  # squeeze() 用于移除单通道的维度
-    plt.axis('off')  # 关闭坐标轴
-    plt.savefig(image_path)  # 保存为 PNG 文件
-    plt.close()
-    # On computer with monitor
-    # img.show()
-    # img.save('work_dirs/tmp.jpg')
-    # tensor = ToTensor()(img)
-    # print(tensor.shape)
-    # print(tensor.max())
-    # print(tensor.min())
+    # plt.imshow(img)  # squeeze() 用于移除单通道的维度
+    # plt.axis('off')  # 关闭坐标轴
+    # plt.savefig(image_path)  # 保存为 PNG 文件
+    # plt.close()
+
+def get_dataloader(batch_size: int):
+    transform = Compose([ToTensor(), Lambda(lambda x: (x - 0.5) * 2)])
+    dataset = torchvision.datasets.MNIST(root='data/mnist',
+                                         transform=transform)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+def get_definite_dataloader(batch_size: int):
+    transform = Compose([ToTensor(), Lambda(lambda x: (x - 0.5) * 2)])
+    dataset = torchvision.datasets.MNIST(root='data/mnist',
+                                         transform=transform)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+def get_img_shape():
+    return (1, 28, 28)
 
 if __name__ == '__main__':
     download_dataset()
