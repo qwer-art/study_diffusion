5d3c86663d656be439bacd50c3eb68c731a52fb9
liyun <1062428799@qq.com>
2025-01-07 00:31:22 +0800

    准备好测试duffision前向和后向的过程了

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ A module that was compiled using NumPy 1.x cannot be run in NumPy 2.2.0 as it ma
 pip uninstall numpy
 pip install numpy==1.22.0
 ```
+### gitkraken
+https://blog.csdn.net/jiang_huixin/article/details/128643351
--- visual_model/viewer_tensor.py
+++ visual_model/viewer_tensor.py
@@ -1,36 +1,53 @@
+import os.path as osp
+import sys
+sys.path.append(osp.dirname(osp.dirname(osp.abspath(__file__))))
+
 import cv2
 import einops
 import numpy as np
+from ddpm.dataset import get_definite_dataloader
+from ddpm.ddpm_model import DDPM
 import torch
-import torch.nn as nn
 
-def tensor_to_images(imgs):
-    if (3 == len(imgs.shape) and 3 == imgs.shape[0]):
-        imgs = einops.rearrange(imgs, 'c h w -> h w c')
-    elif (3 == len(imgs.shape) and 3 != imgs.shape[0]):
-        imgs = imgs.unsqueeze(1)  # 通过 unsqueeze 扩展成 (12, 1, 28, 28)
-        imgs = imgs.expand(-1, 3, -1, -1)
-        n = imgs.shape[0]
+def tensor_to_image(imgs_tensor):
+    if (3 == len(imgs_tensor.shape) and 3 == imgs_tensor.shape[0]):
+        imgs_tensor = einops.rearrange(imgs_tensor, 'c h w -> h w c')
+    elif (3 == len(imgs_tensor.shape) and 3 != imgs_tensor.shape[0]):
+        imgs_tensor = imgs_tensor.unsqueeze(1)  # 通过 unsqueeze 扩展成 (12, 1, 28, 28)
+        imgs_tensor = imgs_tensor.expand(-1, 3, -1, -1)
+        n = imgs_tensor.shape[0]
         b1 = int(n ** 0.5)
         b2 = n // b1
-        imgs = einops.rearrange(imgs, '(b1 b2) c h w -> (b1 h) (b2 w) c', b1=b1, b2=b2)
-    elif (4 == len(imgs.shape)):
-        n = imgs.shape[0]
+        imgs_tensor = einops.rearrange(imgs_tensor, '(b1 b2) c h w -> (b1 h) (b2 w) c', b1=b1, b2=b2)
+    elif (4 == len(imgs_tensor.shape)):
+        n = imgs_tensor.shape[0]
         b1 = int(n ** 0.5)
         b2 = n // b1
-        imgs = einops.rearrange(imgs, '(b1 b2) c h w -> (b1 h) (b2 w) c', b1=b1, b2=b2)
+        imgs_tensor = einops.rearrange(imgs_tensor, '(b1 b2) c h w -> (b1 h) (b2 w) c', b1=b1, b2=b2)
     else:
-        print(f"[ Fail ],shape_size: {imgs.shape}")
+        print(f"[ Fail ],shape_size: {imgs_tensor.shape}")
 
-    imgs = (imgs + 1) / 2 * 255
-    imgs = imgs.clamp(0, 255)
-    imgs = imgs.numpy().astype(np.uint8)
-    return imgs
+    imgs_tensor = (imgs_tensor + 1) / 2 * 255
+    imgs_tensor = imgs_tensor.clamp(0, 255)
+    imgs_tensor = imgs_tensor.numpy().astype(np.uint8)
+    return imgs_tensor
+
 
 if __name__ == '__main__':
-    img_shape = (81, 3, 28, 28)
-    imgs = torch.randn(img_shape)
-    imgs = tensor_to_images(imgs)
+    device = 'cuda'
+    batch_size = 81
+    n_steps = 1000
 
-    cv2.imshow("imgs", imgs)
-    cv2.waitKey(-1)
+    dataloader = get_definite_dataloader(batch_size)
+    x, _ = next(iter(dataloader))
+    x = x.to(device)
+    ddpm = DDPM(device, n_steps)
+    for t in range(n_steps):
+        if (0 == (t % 100)):
+            image_path = f"image_forward/{t}.jpg"
+            t = torch.tensor([t])
+            t = t.unsqueeze(1)
+            print(f"t: {t},{t.shape}")
+            x_t = ddpm.sample_forward(x, t)
+            image = tensor_to_image(x_t.cpu())
+            cv2.imwrite(image_path,image)
